9137306a3f25fea6052b64cfb9f972338458de42
codenashwan <nashwan.via.ps3@gmail.com>
2025-02-01 00:07:45 +0300

    v1.1.5

--- src/OtpiqServiceProvider.php
+++ src/OtpiqServiceProvider.php
@@ -8,7 +8,7 @@ class OtpiqServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/otpiq.php', 'otpiq'
+            __DIR__ . '../config/otpiq.php', 'otpiq'
         );
         $this->app->singleton(OtpiqService::class, function ($app) {
             return new OtpiqService(config('otpiq.api_key'), config('otpiq.base_url'));
